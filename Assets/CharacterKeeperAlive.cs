7fc47106772390a8d920af7f056cc1ed7f253fea
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-27 15:40:19 +0100

    Commented out rotation as moves character around cog

--- Assets/CharacterKeeperAlive.cs
+++ Assets/CharacterKeeperAlive.cs
@@ -10,6 +10,8 @@ public class CharacterKeeperAlive : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 1);
+		//gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 1);
+		//var z = gameObject.transform.localRotation.z;
+		//gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, z+90);
 	}
 }
