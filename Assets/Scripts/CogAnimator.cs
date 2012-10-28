7ba03b76239d5f5f3b69dac461c880c2e891661d
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-28 23:29:53 +0000

    Changed CogAnimator to slow down cog when character hits pickup

--- Assets/Scripts/CogAnimator.cs
+++ Assets/Scripts/CogAnimator.cs
@@ -10,11 +10,25 @@ public class CogAnimator : MonoBehaviour {
 	public GameObject ui;
 	public bool stopped = false;
 	
+	private int timing = 100;
+	
 	// Update is called once per frame
 	void Update () {
-		if (HaveStarted() && !stopped) {
+		if (amountToRotate == 0) {
+			return;
+		}
+		
+		if (HaveStarted()) {
 			// Used negative speed to rotate in opposite direction
-			if ((DateTime.Now.Millisecond / 100) % 5 == 0) {
+			if ((DateTime.Now.Millisecond / timing) % 5 == 0) {
+				if (stopped) {
+					timing = 1;
+					amountToRotate /= 2;
+					if (Math.Round(Mathf.Round(amountToRotate * 1000)) == 0) {
+						amountToRotate = 0;	
+					}
+				}
+				
 				cog.RotateAround(Vector3.zero, Vector3.up, amountToRotate);
 			}
 		}
