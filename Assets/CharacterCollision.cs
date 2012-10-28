4c949b2b91488fbd20aede9bb31b61e038d65a5d
Helen Williamson <lenchkin@gmail.com>
2012-10-28 21:12:29 +0000

    Changed so that pickups are removed when they're collided with

--- Assets/CharacterCollision.cs
+++ Assets/CharacterCollision.cs
@@ -3,21 +3,8 @@ using System.Collections;
 
 public class CharacterCollision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	//void OnCollisionEnter (Collision colidedWith) {
-	//	Debug.Log("Hit something: " + colidedWith.gameObject.name);
-	//}
-	
 	void OnCollisionEnter(Collision colidedWith) {
 		Debug.Log("Hit something: " + colidedWith.gameObject.name);
-	}
-	
-	void OnTriggerEnter(Collider otherObj) {
-			Debug.Log("Hit something: " + otherObj.gameObject.name);
+		Destroy(colidedWith.gameObject);
 	}
 }
