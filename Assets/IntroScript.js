a6e0649fe8ccfd56f039979a9104e53edde135b3
Helen Williamson <lenchkin@gmail.com>
2012-10-28 20:45:45 +0000

    Removed unused code from IntroScript

--- Assets/IntroScript.js
+++ Assets/IntroScript.js
@@ -8,35 +8,12 @@ var btnPlay : Texture2D;
 var showIntro : boolean;
 
 function OnGUI() {
-	
 	//show intro/outro		
 	if (showIntro == true) {
-	
 		GUI.Label(Rect(50, 0,900,900), bgIntro);
-		
-		
-		//if (GUI.Button (Rect (64,380,512,128), btnPlay)) {
-			//showIntro = false;
-			//startGame();
-		//}
 		
 		if (Event.current.type == EventType.KeyDown) {
         	showIntro = false;
-			startGame();
-    	}
-						
+    	}	
 	} 
-	
-	
-}
-
-function Update() {
-	
-}
-
-//start game
-function startGame() {
-	
-
-	
 }
