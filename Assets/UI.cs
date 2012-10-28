1c0d4a96227cbf38b849f970e231bd273a0b2ce7
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-28 21:29:54 +0000

    Added UI label which is displayed once the intro screen is removed

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -5,15 +5,24 @@ public class UI : MonoBehaviour {
 	
 	public Texture2D introBackground;
 	public bool showIntro;
+	public int numberCollected = 0;
 
 	void OnGUI () {
-		if (showIntro == true) {
+		if (showIntro) {
 			GUI.Label(new Rect(50, 0,900,900), introBackground);
 		
 			if (Event.current.type == EventType.KeyDown) {
         		showIntro = false;
     		}	
-		} 
+		}
+		
+		if (!showIntro) {
+			updateUI();	
+		}
+	}
+	
+	void updateUI() {
+		GUI.Box(new Rect(10,10,100,30), "Collected: " + numberCollected);
 	}
 	
 }
