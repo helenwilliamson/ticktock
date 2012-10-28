09a88630af7772ffe7c0702053959263d8fa6d02
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-28 21:21:19 +0000

    Converted IntroScript from javascript to C#

--- Assets/CharacterCollision.cs
+++ Assets/CharacterCollision.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class CharacterCollision : MonoBehaviour {
 
 	void OnCollisionEnter(Collision colidedWith) {
-		Debug.Log("Hit something: " + colidedWith.gameObject.name);
 		Destroy(colidedWith.gameObject);
 	}
 }
