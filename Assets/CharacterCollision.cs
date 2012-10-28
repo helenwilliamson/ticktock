ec2cce8fe98249392f274a3bc7d0ec29eb4f6e5b
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-28 21:37:54 +0000

    Increase counter in ui if pickup collided with

--- Assets/CharacterCollision.cs
+++ Assets/CharacterCollision.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class CharacterCollision : MonoBehaviour {
+	
+	public GameObject ui;
 
 	void OnCollisionEnter(Collision colidedWith) {
 		Destroy(colidedWith.gameObject);
+		var component = ui.GetComponent<UI>();
+		component.numberCollected += 1;
 	}
 }
