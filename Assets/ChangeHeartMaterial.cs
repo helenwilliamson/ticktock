5bbe45941a87bbeee27525571e9331c6f52e8095
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-28 20:33:27 +0000

    Changed material for heart. Added shader which switches between them.

--- Assets/ChangeHeartMaterial.cs
+++ Assets/ChangeHeartMaterial.cs
@@ -5,17 +5,22 @@ using System;
 public class ChangeHeartMaterial : MonoBehaviour {
 	
 	public Material[] materials;
+	public float duration =1f;
 	
 	// Update is called once per frame
 	void Update () {
-		var currentMaterial = renderer.material;
-		if ((DateTime.Now.Millisecond / 100) % 5 == 0) {
-			if (materials[0] == currentMaterial) {
-				renderer.material = materials[1];
-			}
-			else {
-				renderer.material = materials[0];
-			}
-		}
+		//var currentMaterial = renderer.material;
+		//if ((DateTime.Now.Millisecond / 100) % 5 == 0) {
+		//	float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		//	if (materials[0] == currentMaterial) {
+        //		renderer.material.Lerp(materials[0], materials[1], lerp);
+		//	}
+		//	else {
+		//		renderer.material.Lerp(materials[1], materials[0], lerp);
+		//	}
+		//}
+		
+		var lerp = Mathf.PingPong(Time.time, duration) / duration; 
+    	renderer.material.SetFloat("_Blend", lerp );
 	}
 }
