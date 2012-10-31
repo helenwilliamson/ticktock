ff0b2af108145cca979bf387f0759cfaf03697c5
Helen Williamson <helen.williamson@mindcandy.com>
2012-10-31 19:40:21 +0000

    Added material to obstacles. Added obstacles to medium and small cogs. Changed how obstacles and pickups are spawned to habe specific values for position

--- Assets/Scripts/CharacterCollision.cs
+++ Assets/Scripts/CharacterCollision.cs
@@ -31,7 +31,7 @@ public class CharacterCollision : MonoBehaviour {
 	}
 	
 	IEnumerator showFinishScreen() {
-		yield return new WaitForSeconds(5);
+		yield return new WaitForSeconds(2);
 		getUIScript().finished = true;
 	}
 	
--- Assets/Scripts/PickupSpawner.cs
+++ Assets/Scripts/PickupSpawner.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PickupSpawner : MonoBehaviour {
 	
 	public Transform collectiblePrefab;
-	public int numberOfCollectibles;
+	public List<int> positions;
 	public Vector3 startingPosition;
 	public float scale;
 	
 	void Start() {
-		var amountToAdd = 360f / numberOfCollectibles;
-		var ratioAmountToAdd = amountToAdd*0.6f;
 		var initialPosition = transform.localPosition;
 		initialPosition.x = startingPosition.x;
 		initialPosition.y = startingPosition.y;
 		initialPosition.z = startingPosition.z;
 		
-		for (int i = 1; i < numberOfCollectibles; i++) {
+		foreach (int amountToAdd in positions) {
 			Transform collectible = (Transform)Instantiate(collectiblePrefab);
 			collectible.transform.parent = gameObject.transform;
 			collectible.localPosition = startingPosition;
 			collectible.localScale = new Vector3(scale, 0.1f, scale);
 			
-			collectible.RotateAround(Vector3.zero, Vector3.up, (amountToAdd*i)+Random.Range(-ratioAmountToAdd, ratioAmountToAdd));
+			collectible.RotateAround(Vector3.zero, Vector3.up, amountToAdd);
 		}
 	}
 }
